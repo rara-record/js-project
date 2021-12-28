0c27faf0ce4aec351ebb8a553dd048082ff9bd4d
bora <a52447879@gamil.com>
2021-12-28 21:04:49 +0900

    🚧 메인 비주얼 수정

--- js/_visual.js
+++ js/_visual.js
@@ -1,40 +1,25 @@
-const visual = document.querySelector('#visual');
-visual.classList.add('show');
+// const visual = document.querySelector('#visual');
+// visual.classList.add('show');
 
+const initImgContainer = document.querySelector('.banner-img')
+const intImg = initImgContainer.querySelector('img')
+const initImgSrc = intImg.getAttribute('src')
+intImg.style.opacity = '1'
+const strip = document.querySelectorAll('.strip')
 
+strip.forEach((item) => {
+  item.addEventListener('mouseover', function (e) {
+    e.preventDefault()
 
-// const sections = document.querySelectorAll('section');
+    let currentImage = item.dataset.image
+    console.log(currentImage)
+    intImg.setAttribute('src', `./assets/images/${currentImage}`)
+    intImg.style.opacity = '1'
+  })
 
-// // section들의 offset값을 저장할 빈 배열을 만든다.
-// const offsetArray = [];
+  item.addEventListener('mouseout', function (e) {
+    e.preventDefault()
 
-// // 각각의 section을 뽑아서 배열에 넣어준다.
-// for (let item of sections) {
-//   offsetArray.push(item.offsetTop);
-
-// }
-// console.log(offsetArray[0].offsetTop)
-
-
-// window.addEventListener('scroll', e => {
-//   // 현재 스크롤 거리 값을 변수에 저장한다.
-//   let scrollTop = window.scrollY || window.pageYOffset;
-//   // 아이템이 보이는 시점 설정
-//   const baseLine = -200;
-
-//   // 각 섹션에 아이템들을 활성화 시키는 함수를 실행한다.
-//   for (let i = 0; i < sections.length; i++) {
-//     if (scrollTop >= offsetArray[i] + baseLine) {
-//       activate(sections, i);
-//     }
-//   }
-// })
-
-// // 활성화 함수
-// function activate(array, index) {
-//   array.forEach(item => {
-//     item.classList.remove('on');
-//   });
-
-//   array[index].classList.add('on');
-// }
+    intImg.style.opacity = '0.8'
+  })
+})
